59a5fb125b7ef2441c9499fa3adefcdaf5a0345c
amuseic <pk2dpvp@hotmail.com>
2018-10-03 12:32:23 +0200

    sprite engine en dingen

--- WPF Game/Game Engine/Environment/Tile.cs
+++ WPF Game/Game Engine/Environment/Tile.cs
@@ -1,20 +1,37 @@
 ﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace GameEngine
 {
+
     public class Tile : PhysicalObject
     {
+
+        // Use `using` blocks for GDI objects you create, so they'll be released
+        // quickly when you're done with them.
+
         //boolean represents if the Tile is a standable Tile
         public readonly bool Ground;
-        //instances tile given reffered sprite, position, fatness:), and standability
-        public Tile(ref Image Sprite, int x, int y, int width, int height, bool standable = false)
+
+        //instances tile given referred sprite, position, fatness:), and standability
+        public Tile(ref Image Sprite, int x, int y, int height, int widthRepeater, bool standable = false)
         {
+            int destWidth = widthRepeater * 32;
             X = x;
             Y = y;
-            Width = width;
+            Width = destWidth;
             Height = height;
-            collision = new Rectangle((int) X, (int) Y, Width, Height);
-            this.Sprite = Sprite;
+            collision = new Rectangle((int) X, (int) Y, destWidth, Height);
+            Image destImage = (Image) new Bitmap(destWidth, 32);
+            
+            using (TextureBrush brush = new TextureBrush(Sprite, WrapMode.Tile))
+            using (Graphics g = Graphics.FromImage(destImage))
+            {
+                g.FillRectangle(brush, 0, 0, destImage.Width, destImage.Height);
+            }
+            
+            this.Sprite = destImage;
             Ground = standable;
         }
     }
