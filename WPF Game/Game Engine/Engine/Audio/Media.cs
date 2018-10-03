0dbfcd3698f1bbd3081dfb71e1572b08dec99e5e
Velikss <felixbeuving@live.nl>
2018-10-03 12:50:09 +0200

    Added Media class

---
+++ WPF Game/Game Engine/Engine/Audio/Media.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPF_Game.Game_Engine.Engine.Audio
+{
+    class Media
+    {
+        public string location;
+
+        public Media(string location)
+        {
+            this.location = location;
+            //new Thread((ThreadStart)delegate
+           //{
+           //};
+        }
+    }
+}
