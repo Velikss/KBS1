0e9b7eef73272fdf98ea2f388981d76201c1d284
FrozenSource <nijenkamp.b@gmail.com>
2018-10-04 11:15:35 +0200

    Performance fix && parameter fixes

--- WPF Game/Game Engine/Environment/Tile.cs
+++ WPF Game/Game Engine/Environment/Tile.cs
@@ -1,13 +1,10 @@
 ﻿using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 
 namespace GameEngine
 {
-
     public class Tile : PhysicalObject
     {
-
         // Use `using` blocks for GDI objects you create, so they'll be released
         // quickly when you're done with them.
 
@@ -15,30 +12,19 @@ namespace GameEngine
         public readonly bool Ground;
 
         //instances tile given referred sprite, position, fatness:), and standability
-        public Tile(ref Image Sprite, int x, int y, int height, int widthRepeater, bool standable = false)
+        public Tile(ref Image Sprite, int x, int y, int widthRepeater, int height = 32, bool standable = false)
         {
-            int destWidth = widthRepeater * 32;
             X = x;
             Y = y;
-            Width = destWidth;
+            Width = widthRepeater * 32;
             Height = height;
-            collision = new Rectangle((int) X, (int) Y, destWidth, Height);
+            collision = new Rectangle((int) X, (int) Y, Width, Height);
             if (widthRepeater > 1)
-            {
-                Image destImage = (Image) new Bitmap(destWidth, 32);
-
-                using (TextureBrush brush = new TextureBrush(Sprite, WrapMode.Tile))
-                using (Graphics g = Graphics.FromImage(destImage))
-                {
-                    g.FillRectangle(brush, 0, 0, destImage.Width, destImage.Height);
-                }
-
-                this.Sprite = destImage;
-            }
+                using (TextureBrush brush = new TextureBrush(this.Sprite = new Bitmap(Width, 32), WrapMode.Tile))
+                using (Graphics g = Graphics.FromImage(this.Sprite))
+                    g.FillRectangle(brush, 0, 0, this.Sprite.Width, this.Sprite.Height);
             else
-            {
                 this.Sprite = Sprite;
-            }
 
             Ground = standable;
         }
