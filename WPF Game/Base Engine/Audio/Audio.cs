01d6eb2e141037125d6a390a12540dd4cc3f65f3
amuseic <pk2dpvp@hotmail.com>
2018-10-09 14:18:17 +0200

    Deleted some large docs files

--- WPF Game/Base Engine/Audio/Audio.cs
+++ WPF Game/Base Engine/Audio/Audio.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Media;
-using NAudio.Wave;
+//using NAudio.Wave;
 
 // ReSharper disable UnusedMember.Local
 
@@ -8,20 +8,21 @@ namespace BaseEngine
 {
     public class Audio
     {
-        private MediaFoundationReader audioFile;
-        private WaveOutEvent outputDevice;
-        private Dictionary<string, MediaPlayer> Soundtrack = new Dictionary<string, MediaPlayer>();
-
-        public void AddMedia(string Name, string Location)
-        {
-            if (outputDevice == null) outputDevice = new WaveOutEvent();
-            if (audioFile == null)
-            {
-                audioFile = new MediaFoundationReader(@"C:\Users\usr\Downloads\1.wav");
-                outputDevice.Init(audioFile);
-            }
-
-            outputDevice.Play();
-        }
+//        private MediaFoundationReader audioFile;
+//        private WaveOutEvent outputDevice;
+//        private Dictionary<string, MediaPlayer> Soundtrack = new Dictionary<string, MediaPlayer>();
+//
+//        public void AddMedia(string Name, string Location)
+//        {
+//            if (outputDevice == null) outputDevice = new WaveOutEvent();
+//            if (audioFile == null)
+//            {
+//                audioFile = new MediaFoundationReader(@"C:\Users\usr\Downloads\1.wav");
+//                outputDevice.Init(audioFile);
+//            }
+//
+//            outputDevice.Play();
+//        }
     }
+
 }
