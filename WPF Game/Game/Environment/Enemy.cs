ec83969781f690ff5a3bc9ea469a0b22e11d81be
amuseic <pk2dpvp@hotmail.com>
2018-10-12 12:03:46 +0200

    broken enemy

--- WPF Game/Game/Environment/Enemy.cs
+++ WPF Game/Game/Environment/Enemy.cs
@@ -12,7 +12,6 @@ namespace GameEngine
         private int stopFollowingAt; //Location Y from visible -until player.Y + (stopFollowingAt + remaining space to the right of the screen)
         private bool visible;
         private Camera camera;
-        private Image normal, normal_L;
 
         #endregion
 
@@ -22,51 +21,7 @@ namespace GameEngine
             X = x;
             Y = y;
             camera = cam;
-            normal = Image.FromFile("Animations/enemy/enemy.gif");
-            Sprite = normal;
-            normal_L = Image.FromFile("Animations/enemy/enemy_L.gif");
-            //new Thread(EnemyAnimation).Start();
-        }
-
-        private void EnemyAnimation()
-        {
-            //movement sprite changer to give walking animation
-            var sequence_number = 1;
-            //keeps last direction for jump_direction
-            var last_direction = 2;
-            //checks if to switch sequence number
-            var switching = true;
-            while (true)
-            {
-                if (sequence_number == 1)
-                    switching = true;
-                if (sequence_number == 3)
-                    switching = false;
-                if (switching)
-                    sequence_number++;
-                else
-                    sequence_number--;
-                if (camera.Up)
-                {
-                    if (camera.Left)
-                        last_direction = 1;
-                    if (camera.Right)
-                        last_direction = 2;
-                }
-
-                if (!camera.Left && !camera.Right && !camera.Up && !camera.Down)
-                    switch (last_direction)
-                    {
-                        case 1:
-                            Sprite = normal_L;
-                            break;
-                        case 2:
-                            Sprite = normal;
-                            break;
-                    }
-                
-                Thread.Sleep(100);
-            }
+            Sprite = Image.FromFile("Animations/enemy/enemy.gif");
         }
         
     }
