244a75713d3e7a1750eff71b565bdc7375e082e4
Velikss <felixbeuving@live.nl>
2018-10-12 13:46:53 +0200

    0.5.4.1

---
+++ WPF Game/Game/ScoreController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Game.Game
+{
+    class ScoreController
+    {
+
+
+        public void SaveScoreToXML(string TimeStamp, int Score, string Level)
+        {
+
+        }
+    }
+}
